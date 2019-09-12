b131f0caa64a9f0f76705caf2345563f47403b34
Angel Burgos <Gabriel@192.168.6.67>
2019-09-12 17:35:01 -0400

    Changed Background color to purple (MAGENTA).

--- src/Display/DisplayScreen.java
+++ src/Display/DisplayScreen.java
@@ -34,7 +34,7 @@ public class DisplayScreen {
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        frame.setBackground(Color.black);
+        frame.setBackground(Color.MAGENTA);
 
         try {
             frame.setIconImage(ImageIO.read(new File("res/Sheets/icon.png")));
@@ -47,7 +47,7 @@ public class DisplayScreen {
         canvas.setMaximumSize(new Dimension(width, height));
         canvas.setMinimumSize(new Dimension(width, height));
         canvas.setFocusable(false);
-        canvas.setBackground(Color.black);
+        canvas.setBackground(Color.MAGENTA);
 
         frame.add(canvas);
         frame.pack();

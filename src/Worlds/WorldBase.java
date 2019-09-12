ce72e56b808943f4e595580d8d3061058e07a26a
Angel Burgos <Gabriel@192.168.6.67>
2019-09-12 17:39:42 -0400

    Fixed grid lines visibility issue. Now grid lines are not visible.

--- src/Worlds/WorldBase.java
+++ src/Worlds/WorldBase.java
@@ -55,9 +55,9 @@ public abstract class WorldBase {
 
         for (int i = 0; i <= 800; i = i + GridPixelsize) {
 
-           
-            g.drawLine(0, i, handler.getWidth() , i);
-            g.drawLine(i,0,i,handler.getHeight());
+        	//g.setColor(Color.white);
+        	//g.drawLine(0, i, handler.getWidth() , i);
+        	//g.drawLine(i,0,i,handler.getHeight());
 
         }
 

b2439558747b0087856a0777e7de9f42bb6e800d
Calvin Cima de Villa <calvin.cimadevilla@upr.edu>
2019-09-13 19:22:26 -0400

    Implemented key "ESC" to make the game enter the pause menu.

--- src/Game/GameStates/GameState.java
+++ src/Game/GameStates/GameState.java
@@ -37,6 +37,9 @@ public class GameState extends State {
     public void tick() {
 
         handler.getWorld().tick();
+        if(handler.getKeyManager().pbutt) {
+        	State.setState(handler.getGame().pauseState);
+        }
 
     }
 

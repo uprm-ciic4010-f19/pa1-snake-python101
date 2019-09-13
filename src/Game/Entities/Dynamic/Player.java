9a4e2ad2b1132f30de8e6bfd7e8596661c7bca1b
Calvin Cima de Villa <calvin.cimadevilla@upr.edu>
2019-09-13 18:32:16 -0400

    Backtracking prevented

--- src/Game/Entities/Dynamic/Player.java
+++ src/Game/Entities/Dynamic/Player.java
@@ -40,13 +40,13 @@ public class Player {
 			checkCollisionAndMove();
 			moveCounter=3;
 		}
-		if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_UP)){
+		if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_UP)&& direction != "Down"){
 			direction="Up";
-		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_DOWN)){
+		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_DOWN)&& direction != "Up"){
 			direction="Down";
-		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_LEFT)){
+		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_LEFT)&& direction != "Right"){
 			direction="Left";
-		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_RIGHT)){
+		}if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_RIGHT)&& direction != "Left"){
 			direction="Right";
 		}   
 		if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_MINUS)){
